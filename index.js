a265e1a3d5e12d8e9b3459090f6352f1e768e6c6
Tracy <tracylmichaels@gmail.com>
2021-08-12 16:07:45 -0400

    add initial features

---
+++ index.js
@@ -0,0 +1,372 @@
+const { executionAsyncResource } = require('async_hooks');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const rhyme = require('rhyme');
+const synonyms = require('synonyms');
+const tcom = require('thesaurus-dom');
+const date = require('date-and-time');
+
+/********************************************************************
+ * This bot was created for the Discord server of FrenZone
+ * 
+ * author: tracy
+ * 
+ * 
+ ********************************************************************/
+
+//data
+const {
+    prefix,
+    token,
+} = require('./config.json');
+const emojiList = require('./emojiList.json');
+const cardList = require('./cards.json');
+const reactionMIN = 5;
+const reactionMAX = 15;
+const sarcasticMIN = 20;
+const sarcasticMAX = 100;
+const musicQueue = new Map();
+var messageCounter = 0;
+var reactionCounter = 0;
+var sarcasticThreshold = getRandomIntInclusive(sarcasticMIN, sarcasticMAX);
+var reactionThreshold = getRandomIntInclusive(reactionMIN, reactionMAX);
+
+
+client.on("ready", () => {
+    console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+    //ignore messages from bot
+    if (msg.author.bot) return;
+    //activity log
+    try {
+        console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag} sent a message to ${msg.channel.name}`);
+    } catch (e) {
+        console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: log failed`);
+    }
+
+    //reacts skull emoji on "I forgor"
+    if (msg.content.toLowerCase().match(new RegExp("i forgor")) != null) msg.react('💀');
+
+    //return if not in approved non-serious channels, only during initial building and testing
+    //if (msg.channel.name != "grill-n-chill" && msg.channel.name != 'bot-sandbox' && msg.channel.name != "bot-feature-requests") return;
+
+    // messageCounter++;
+    reactionCounter++;
+    // console.log("Message Counter: " + messageCounter);
+    //console.log("Reaction Counter: " + reactionCounter);
+    // console.log("Sarcastic Threshold: " + sarcasticThreshold);
+    //console.log("Reaction Threshold: " + reactionThreshold);
+
+    // //reply with a sarcastic comment
+    // if (messageCounter === sarcasticThreshold) {
+    //     msg.reply(sarcasticComment(msg.content));
+    //     messageCounter = 0;
+    //     sarcasticThreshold = getRandomIntInclusive(sarcasticMIN, sarcasticMAX);
+    // }
+
+    //react randomly to a message with custom emoji
+
+    if (reactionCounter === reactionThreshold) {
+        try {
+            msg.react(msg.guild.emojis.cache.find(emoji => emoji.name === emojiList[Math.floor(Math.random() * emojiList.length)]));
+        } catch (e) {
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: reaction failed`);
+        }
+        reactionCounter = 0;
+        reactionThreshold = getRandomIntInclusive(reactionMIN, reactionMAX);
+    }
+
+    if (!msg.content.startsWith(prefix)) return;
+    var num;
+    var msgContents = msg.content.toLowerCase().trim().split(' ');
+    switch (msgContents[0]) {
+        //test***************************************************************************************************
+        case `${prefix}ping`:
+            msg.reply("stfu");
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            break;
+
+        //fun*****************************************************************************************************
+        //music player
+        case `${prefix}music`:
+            try {
+                const serverQueue = musicQueue.get(msg.guild.id);
+                switch (msgContents[1]) {
+                    case `play`:
+                        console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+                        execute(msg, serverQueue);
+                        msg.delete();
+                        break;
+                    case `skip`:
+                        console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+                        skip(msg, serverQueue);
+                        break;
+                    case `stop`:
+                        console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+                        stop(msg, serverQueue);
+                        break;
+                    default:
+                        msg.channel.send("invalid command");
+                }
+            } catch (e) {
+                console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: music command failed`);
+                
+            }
+            break;
+        //end fun
+
+        //Band utilities***********************************************************************************************
+        //roll a dice, can have parameter for number of sides, default is 6
+        case `${prefix}dice`:
+            if (msgContents.length > 1) {
+                num = rollDice(parseInt(msgContents[1]));
+            } else {
+                num = rollDice(6);
+            }
+            if (!isNaN(num)) {
+                msg.reply(`You rolled a ${num}`);
+                console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            } else {
+                msg.reply('Invalid command: use \"!dice \\d?\" default is 6');
+            }
+            break;
+
+        //arbiter method
+        case `${prefix}arbiter`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            var numCards = 1
+            if (msgContents.length > 1) {
+                numCards = Math.abs(parseInt(msgContents[1]));
+            }
+            var temp = "";
+            if (!isNaN(numCards)) {
+                for (let i = 0; i < numCards; i++) {
+                    temp += cardList[Math.floor(Math.random() * cardList.length)];
+                }
+            } else if (msgContents[1] === 'key') {
+                msg.channel.send(`${fs.readFileSync('./arbiter_key.txt')}`);
+                break;
+            } else {
+                msg.channel.send("invalid parameter");
+                break;
+            }
+            msg.channel.send(temp);
+            break;
+
+        //lyric tools
+        //rhymes
+        case `${prefix}rhyme`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            if (msgContents.length > 1) {
+                rhyme(function (r) {
+                    var rhymes = r.rhyme(msgContents[1]).join(' ').toLowerCase();
+                    if (rhymes === "") {
+                        msg.channel.send("(undefined)");
+                        return;
+                    }
+                    msg.channel.send(rhymes);
+                });
+            } else {
+                msg.channel.send("invalid parameter");
+            }
+            break;
+        //synonyms
+        case `${prefix}synonym`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            if (msgContents.length > 1) {
+                try {
+                    var nouns = synonyms(msgContents[1], "n").join(' ');
+                } catch (e) {
+                    nouns = "(undefined)";
+                }
+                try {
+                    var verbs = synonyms(msgContents[1], "v").join(' ');
+                } catch (e) {
+                    verbs = "(undefined)";
+                }
+                msg.channel.send(`nouns: ${nouns}\nverbs: ${verbs}`);
+            } else {
+                msg.channel.send("needs a parameter");
+            }
+            break;
+        //antonyms ***DOES NOT WORK***
+        case `${prefix}antonym`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            if (msgContents.length > 1) {
+                try {
+                    console.log(tcom.search(msgContents[1]).antonyms.join(' '));
+                } catch (e) {
+                    console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: antonym failed`);
+                }
+            }
+            break;
+        // end utilities
+
+        //pastas********************************************************************************
+        //TODO rework to !pasta <name>
+        //message the navy seal copy posta
+        case `${prefix}sealpasta`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            msg.channel.send(`${fs.readFileSync('./navyseal.txt')}`);
+            break;
+
+        case `${prefix}rmpasta`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            msg.channel.send(`${fs.readFileSync('./rmpasta.txt')}`);
+            break;
+
+        case `${prefix}fgpasta`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            msg.channel.send(`${fs.readFileSync('./fgpasta.txt')}`);
+            break;
+
+        case `${prefix}daddypasta`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            msg.channel.send(`${fs.readFileSync('./daddypasta.txt')}`);
+            break;
+        //speak the navy seal copy pasta
+        case `${prefix}saysealpasta`:
+            msg.channel.send(`${fs.readFileSync('./navyseal.txt')}`, { tts: true });
+            break;
+        //end pastas
+
+        //meta********************************************************************
+        //TODO add meta info
+        //end meta
+
+        //close bot if being too annoying
+        case `${prefix}fuckoff`:
+            console.log(`${date.format(new Date(), 'YYYY/MM/DD HH:mm:ss')}:: ${msg.member.user.tag}: Executed ${msgContents[0]} command`);
+            msg.channel.send("k bye");
+            process.exit();
+    }
+});
+
+// Function called when the "dice" command is issued
+function rollDice(num) {
+    const sides = num;
+    return Math.floor(Math.random() * sides) + 1;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sarcasticComment(message) {
+    var msgArr = message.toLowerCase().split("");
+    for (var i = 0; i < msgArr.length; i++) {
+        if (i % 2 == 0) {
+            msgArr[i] = msgArr[i].toUpperCase();
+        }
+    }
+    return msgArr.join('');
+}
+
+/*************************************************************************************************
+* for music player 
+**************************************************************************************************/
+async function execute(message, serverQueue) {
+    const args = message.content.split(" ");
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+        return message.channel.send(
+            "You need to be in a voice channel to play music!"
+        );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send(
+            "I need the permissions to join and speak in your voice channel!"
+        );
+    }
+
+    const songInfo = await ytdl.getInfo(args[2]);
+    const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+    };
+
+    if (!serverQueue) {
+        const queueContruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 5,
+            playing: true
+        };
+
+        musicQueue.set(message.guild.id, queueContruct);
+
+        queueContruct.songs.push(song);
+
+        try {
+            var connection = await voiceChannel.join();
+            queueContruct.connection = connection;
+            play(message.guild, queueContruct.songs[0]);
+        } catch (err) {
+            console.log(err);
+            musicQueue.delete(message.guild.id);
+            return message.channel.send(err);
+        }
+    } else {
+        serverQueue.songs.push(song);
+        return message.channel.send(`${song.title} added to the queue!`);
+    }
+}
+
+function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+        return message.channel.send(
+            "You have to be in a voice channel to stop the music!"
+        );
+    if (!serverQueue)
+        return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+        return message.channel.send(
+            "You have to be in a voice channel to stop the music!"
+        );
+
+    if (!serverQueue)
+        return message.channel.send("There is no song that I could stop!");
+
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+    const serverQueue = musicQueue.get(guild.id);
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        musicQueue.delete(guild.id);
+        return;
+    }
+
+    const dispatcher = serverQueue.connection
+        .play(ytdl(song.url))
+        .on("finish", () => {
+            serverQueue.songs.shift();
+            play(guild, serverQueue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Started playing: **${song.title}**`);
+}
+/**********************************************************************************************
+ * end music player
+ **********************************************************************************************/
+
+
+client.login(token);
