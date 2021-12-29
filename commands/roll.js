d233d216e82015e4ff3d01b3e1fa3a8ae9bff901
Tracy <tracylmichaels@gmail.com>
2021-12-29 16:55:19 -0500

    migrated roll command

---
+++ commands/roll.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+// defaults
+// var numDice = 1;
+// var numSides = 6;
+// var modifier = 0;
+
+// rolls a dice
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Rolls a dice')
+        .addIntegerOption(option =>
+            option.setName('numdice')
+                .setDescription('Number of dice to roll (Default: 1)')
+        )
+        .addIntegerOption(option =>
+            option.setName('sides')
+                .setDescription('Number of sides on the dice (Default: 6)')
+        )
+        .addIntegerOption(option =>
+            option.setName('mod')
+                .setDescription('Modifier to add to the roll (Default: 0)')
+        ),
+    async execute(interaction) {
+        // parse options
+        const numDice = interaction.options.getInteger('numdice') || 1;
+        const numSides = interaction.options.getInteger('sides') || 6;
+        const modifier = interaction.options.getInteger('mod') || 0;
+
+        // roll dice
+        let total = 0;
+        for (let i = 0; i < numDice; i++) {
+            total += Math.floor(Math.random() * numSides) + 1;
+        }
+
+        // add modifier
+        total += modifier;
+
+        // send result
+        await interaction.reply(`You rolled a ${total}!`);
+    }
+};
