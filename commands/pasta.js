78d309618a8bc178791d747f690df8c121ebf1a9
Tracy <tracylmichaels@gmail.com>
2021-12-30 21:28:01 -0500

    dynamically list available pastas

--- commands/pasta.js
+++ commands/pasta.js
@@ -1,20 +1,27 @@
 const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-var pastas = ["daddy", "familyguy", "linux", "navyseal", "rickmorty"];
+var pastas = fs.readdirSync('./pastas', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        pastas.push(file);
+    });
+});
 
-
+for (let i = 0; i < pastas.length; i++) {
+    pastas[i] = pastas[i].replace('.txt', '');
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('pasta')
-        .setDescription('Display or create(coming soon) a copy pasta')
+        .setDescription('Display a copy pasta')
         .addStringOption(option => {
             option.setName('name')
                 .setDescription('The pasta to display')
                 .setRequired(true);
-                
-            for(var i = 0; i<pastas.length; i++) {
+
+            for (var i = 0; i < pastas.length; i++) {
                 option.addChoice(pastas[i], pastas[i]);
             }
             return option;
