c85e647a888be36bf27b0b12477dc9a7865a8b7c
Tracy <tracylmichaels@gmail.com>
2022-01-01 12:06:20 -0500

    migrate synonym command

---
+++ commands/synonym.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const synonyms = require('synonyms');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('synonym')
+        .setDescription('Get synonyms of a word')
+        .addStringOption(option =>
+            option.setName('word')
+                .setDescription('The word to get synonyms of')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const word = interaction.options.getString('word');
+
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+
+        var nouns = synonyms(word, 'n');
+        var verbs = synonyms(word, 'v');
+
+        var msgContents = `nouns: ${(nouns != undefined) ? nouns.join(', ') : '(undefined)'}
+                            \nverbs: ${(verbs != undefined) ? verbs.join(', ') : '(undefined)'}`;
+        
+        await interaction.editReply(msgContents);
+    }
+}
