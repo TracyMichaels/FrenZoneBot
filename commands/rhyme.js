64a72c110f171b34da4d98f1499d3404e02b40c5
Tracy <tracylmichaels@gmail.com>
2022-01-01 11:54:09 -0500

    migrate rhyme command

---
+++ commands/rhyme.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const rhyme = require('rhyme');
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('rhyme')
+        .setDescription('Rhyme a word')
+        .addStringOption(option =>
+            option.setName('word')
+                .setDescription('The word to rhyme')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const word = interaction.options.getString('word');
+
+        await interaction.deferReply({
+            ephemeral: false,
+        });
+
+        rhyme(function (r) {
+            var rhymes = r.rhyme(word).join(', ').toLowerCase();
+            if (rhymes.length > 0) {                 
+                interaction.editReply({
+                    content: rhymes,
+                });
+            } else {
+                interaction.editReply("No rhymes found");
+            }
+        });
+    }    
+}
+
